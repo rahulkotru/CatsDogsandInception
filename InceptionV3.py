43837553cd8d9616d76850842dd853171cc0ff4f
Rahul <rahulkotr1@gmail.com>
2022-01-07 17:07:15 +0530

    Modify Inception a bit(Ignore)

--- InceptionV3.py
+++ InceptionV3.py
@@ -52,4 +52,3 @@ for layer in pre_trained_model.layers:
 last_layer = pre_trained_model.get_layer('mixed7')
 print('last layer output shape: ', last_layer.output_shape)
 last_output = last_layer.output
-lol=0
