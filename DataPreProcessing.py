ca3583525251c5e1c1f08c71d583d1f1968e0173
Rahul <rahulkotr1@gmail.com>
2022-01-07 15:26:39 +0530

    Change Bool condition for download_dir

--- DataPreProcessing.py
+++ DataPreProcessing.py
@@ -4,8 +4,10 @@ import zipfile
 import os
 
 
-download_dir = 'D:/28_GitHub/Inception V3/tmp/'
-if (os.path.exists(download_dir)==False):
+path='D:/28_GitHub/Inception V3'
+
+download_dir = path+'/tmp/'
+if not (os.path.exists(download_dir)):
     os.mkdir(download_dir)
 
 data_url = "https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip"
@@ -15,5 +17,7 @@ zip_ref = zipfile.ZipFile(data_file_name, 'r')
 zip_ref.extractall(download_dir)
 zip_ref.close()
 
-print("Number of cat images:",len(os.listdir('/tmp/PetImages/Cat/')))
-print("Number of dog images:", len(os.listdir('/tmp/PetImages/Dog/')))
+print("Number of cat images:",len(os.listdir(path+'/tmp/PetImages/Cat/')))
+print("Number of dog images:", len(os.listdir(path+'/tmp/PetImages/Dog/')))
+
+#Creating Train-Test-Split Directories
