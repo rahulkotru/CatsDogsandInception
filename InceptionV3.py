46be8230c67f37b06efa8c7df040b8ab015429da
Rahul <rahulkotr1@gmail.com>
2022-01-07 16:43:04 +0530

    Add Inception Model

---
+++ InceptionV3.py
@@ -0,0 +1,54 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.optimizers import RMSprop
+import urllib
+
+
+
+TRAINING_DIR = "/tmp/cats-v-dogs/training/"
+# Experiment with your own parameters to reach 99.9% validation accuracy or better
+train_datagen = ImageDataGenerator(rescale=1./255,
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    batch_size=100,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                              batch_size=100,
+                                                              class_mode='binary',
+                                                              target_size=(150, 150))
+
+
+weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
+weights_file = "inception_v3.h5"
+urllib.request.urlretrieve(weights_url, weights_file)
+
+# Instantiate the model
+pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
+                                include_top=False,
+                                weights=None)
+
+# load pre-trained weights
+pre_trained_model.load_weights(weights_file)
+
+# freeze the layers
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+# pre_trained_model.summary()
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
