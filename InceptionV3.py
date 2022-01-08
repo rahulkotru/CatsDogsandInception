197378382fecd5a672ce41596b7ed857207344f7
Rahul <rahulkotr1@gmail.com>
2022-01-08 10:25:25 +0530

    Change layer trainable to true

--- InceptionV3.py
+++ InceptionV3.py
@@ -3,53 +3,40 @@ from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.optimizers import RMSprop
+
 import urllib
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
 
 
+def modelnet(weights_file):    # Instantiate the model
+        pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
+                                        include_top=False,
+                                        weights=None)
 
-TRAINING_DIR = "/tmp/cats-v-dogs/training/"
-# Experiment with your own parameters to reach 99.9% validation accuracy or better
-train_datagen = ImageDataGenerator(rescale=1./255,
-      rotation_range=40,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      shear_range=0.2,
-      zoom_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
-train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
-                                                    batch_size=100,
-                                                    class_mode='binary',
-                                                    target_size=(150, 150))
+        # load pre-trained weights
+        pre_trained_model.load_weights(weights_file)
 
-VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
+        # freeze the layers
+        for layer in pre_trained_model.layers:
+            layer.trainable = True
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
-validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
-                                                              batch_size=100,
-                                                              class_mode='binary',
-                                                              target_size=(150, 150))
+        # pre_trained_model.summary()
 
+        last_layer = pre_trained_model.get_layer('mixed7')
+        print('last layer output shape: ', last_layer.output_shape)
+        last_output = last_layer.output
 
-weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
-weights_file = "inception_v3.h5"
-urllib.request.urlretrieve(weights_url, weights_file)
+        x = layers.Flatten()(last_output)
 
-# Instantiate the model
-pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
-                                include_top=False,
-                                weights=None)
+        x = layers.Dense(1024, activation='relu')(x)
 
-# load pre-trained weights
-pre_trained_model.load_weights(weights_file)
+        x = layers.Dense(1, activation='sigmoid')(x)
 
-# freeze the layers
-for layer in pre_trained_model.layers:
-    layer.trainable = False
+        model = Model(pre_trained_model.input, x)
+        
 
-# pre_trained_model.summary()
-
-last_layer = pre_trained_model.get_layer('mixed7')
-print('last layer output shape: ', last_layer.output_shape)
-last_output = last_layer.output
-
+        model.compile(optimizer=RMSprop(lr=0.0001),
+                    loss='binary_crossentropy',
+                    metrics=['acc'])
+        return model
